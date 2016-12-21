9818eba89cff2f92fdcda514842a852d1808fd16
Jim Clark <slimslenderslacks@gmail.com>
2016-12-20 23:25:03 -0800

    actually destructure the token_path as well

--- .atomist/executors/RestartTravisBuild.ts
+++ .atomist/executors/RestartTravisBuild.ts
@@ -8,9 +8,9 @@ class RestartTravisBuild implements Executor {
     parameters: Parameter[] = [{name: "build_id", description: "Build ID", pattern: "^.*$", maxLength: 100, required: true}
                                {name: "token_path", description: "Access Token Path", pattern: "^.*$", required: false,
                                 tags: [{name: "token", description: "valid github token for authenticating with Travis"}]}]
-    execute(services: Services, {build_id} : {build_id: string}): Result {
+    execute(services: Services, {build_id token_path} : {build_id: string, token_path: any}): Result {
         let _services: any = services
-        _services.travis().restart(build_id)
+        _services.travis().restart(build_id, token_path)
 
         return new Result(Status.Success, "OK")
     }
